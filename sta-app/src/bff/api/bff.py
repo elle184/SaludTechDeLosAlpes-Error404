42bea3fceaaabba50e3a1b53ee77d0036f49b9e5
Mayistikar <andersonrodriguezce@gmail.com>
2025-03-10 19:49:23 -0500

    upload apache stand alone

--- sta-app/src/bff/api/bff.py
+++ sta-app/src/bff/api/bff.py
@@ -1,7 +1,7 @@
 import bff.seedwork.presentation.api as api
 import json
 from bff.seedwork.domain.exceptions import DomainException
-from flask import redirect, render_template, request, session, url_for
+from flask import redirect, render_template, request, session, url_for, jsonify
 from flask import Response
 import requests
 
@@ -17,6 +17,26 @@ def ping() :
 def anonymized_data():
     try:
         response = requests.get("http://34.132.113.112:5002/anonimizador/users")
+        response.raise_for_status()
+        data = response.json()
+        return jsonify(data), 200
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+@bp.route('/process-data', methods=['POST'])
+def process_data():
+    try:
+        payload = request.get_json()
+        print(payload)
+        response = requests.post(
+            "http://localhost:5000/saga",
+            headers={
+                "accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            json=payload
+        )
+        print(response.json())
         response.raise_for_status()
         data = response.json()
         return jsonify(data), 200
